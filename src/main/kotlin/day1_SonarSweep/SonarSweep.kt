87c5a8f70ea868fc246e98b2efa4ca898e888552
Chrisjobling <chris.jobling@salecycle.com>
2022-08-17 12:03:10 +0100

    Day one part 2 complete

--- src/main/kotlin/day1_SonarSweep/SonarSweep.kt
+++ src/main/kotlin/day1_SonarSweep/SonarSweep.kt
@@ -5,18 +5,28 @@ import utility.readFile
 fun main() {
     val fileName = "day1.txt"
     val input = readFile(fileName)
-    println(sonarSweep(input))
+    val intInput =input.map { it.toInt() }
+    println(Day1().part1(intInput))
+    println(Day1().part2(intInput))
 }
 
-fun sonarSweep(input: List<String>): Int {
-    var biggerMeasurements = 0
-    input.forEachIndexed { index, s ->
-        if (index != 0 && s.toInt() > input[index - 1].toInt()) {
-            biggerMeasurements += 1
+class Day1 {
+    fun part1(input: List<Int>): Int {
+        var biggerMeasurements = 0
+        input.forEachIndexed { index, s ->
+            if (index != 0 && s > input[index - 1]) {
+                biggerMeasurements += 1
+            }
         }
+        return biggerMeasurements
     }
-    return biggerMeasurements
 
+    fun part2(input: List<Int>): Int {
+        return part1(
+            input.windowed(size = 3, step = 1)
+            .map { it.sum() }
+        )
+    }
 }
 
 
--- src/test/kotlin/day1_SonarSweep/SonarSweepKtTest.kt
+++ src/test/kotlin/day1_SonarSweep/SonarSweepKtTest.kt
@@ -1,14 +1,22 @@
 package day1_SonarSweep
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 internal class SonarSweepKtTest {
     @Test
     fun sonarSweepPart1IsValid() {
-        assertEquals(7, sonarSweep( listOf("199","200","208","210","200","207","240","269","260","263")))
-        assertEquals(0, sonarSweep( listOf("10","9","8","7","6","5","4","3","2","1")))
-        assertEquals(1, sonarSweep( listOf("10","9","8","7","99","5","4","3","2","1")))
+        val day1 = Day1()
+        assertEquals(7, day1.part1(listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)))
+        assertEquals(0, day1.part1(listOf(10,9,8,7,6,5,4,3,2,1)))
+        assertEquals(1, day1.part1(listOf(10,9,8,7,6,99,4,3,2,1)))
+    }
+
+    @Test
+    fun sonarSweepPart2IsValid() {
+        val day1 = Day1()
+        assertEquals(1, day1.part2(listOf(199,200,666,399,188,377,288)))
+        assertEquals(3, day1.part2(listOf(1,1,2,2,3,4)))
+        assertEquals(0, day1.part2(listOf()))
     }
 }
